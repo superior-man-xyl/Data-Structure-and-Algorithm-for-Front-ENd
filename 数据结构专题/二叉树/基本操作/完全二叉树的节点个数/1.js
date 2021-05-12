093aafcebc6d71cfad7c4bf03774c62e08860334
superior-man-xyl <15170725069@163.com>
2021-05-12 19:44:53 +0800

    二叉树

---
+++ 数据结构专题/二叉树/基本操作/完全二叉树的节点个数/1.js
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+//最简易粗暴的做法
+ var countNodes = function(root) {
+    if(root==null){
+        return 0;
+    }
+    return countNodes(root.left)+countNodes(root.right)+1;
+};

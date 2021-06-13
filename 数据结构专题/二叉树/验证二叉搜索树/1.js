c2247122077b55f49ff7e69573542ce927c5b17b
superior-man-xyl <15170725069@163.com>
2021-06-13 19:56:50 +0800

    验证搜索二叉树

---
+++ 数据结构专题/二叉树/验证二叉搜索树/1.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+//使用中序遍历,中序遍历特点就是后一个值一定大于前一个的值
+var isValidBST = function (root) {
+    let stack = [];
+    let inorder = -Infinity;
+    while (stack.length || root !== null) {
+        while (root !== null) {
+            stack.push(root);
+            root = root.left;
+        }
+        root = stack.pop();
+        // 如果中序遍历得到的节点的值小于等于前一个 inorder，说明不是二叉搜索树
+        if (root.val <= inorder) {
+            return false;
+        }
+        inorder = root.val;
+        root = root.right;
+    }
+    return true;
+};
+
+//时间和空间复杂度皆为O(n)，即n个节点
---
+++ 数据结构专题/二叉树/验证二叉搜索树/2.js
@@ -0,0 +1,13 @@
+//递归实现，用时击败90%
+const helper = (root, lower, upper) => {
+    if (root === null) {
+        return true;
+    }
+    if (root.val <= lower || root.val >= upper) {
+        return false;
+    }
+    return helper(root.left, lower, root.val) && helper(root.right, root.val, upper);
+}
+var isValidBST = function (root) {
+    return helper(root, -Infinity, Infinity);
+};
